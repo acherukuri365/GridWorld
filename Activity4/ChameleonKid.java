3654f245092edf4f62068bc4bd27021497380562
acherukuri365 <122913915+acherukuri365@users.noreply.github.com>
2023-03-23 10:01:07 -0700

    Add files via upload

---
+++ Activity4/ChameleonKid.java
@@ -0,0 +1,18 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Location;
+import java.util.ArrayList;
+
+public class ChameleonKid extends ChameleonCritter {
+	public ArrayList<Actor> getActors()
+    {
+		ArrayList<Actor> actors = new ArrayList<Actor>();
+		
+		Actor actor = getGrid().get(getLocation.getAdjacentLocation(Location.NORTH));
+		if(actor != null) actors.add(actor);
+		
+		actor = getGrid().get(getLocation.getAdjacentLocation(Location.SOUTH));
+		if(actor != null) actors.add(actor);
+		
+        return actors;
+    }
+}
